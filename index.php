9393eec8c1cac1fb9e7062c7c3a17ba3b8127226
walts731 <142887725+walts731@users.noreply.github.com>
2025-06-11 07:18:32 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pilar Asset Inventory</title>
+</head>
+<body>
+    <h1>Hello Pilar!</h1>
+</body>
+</html>
